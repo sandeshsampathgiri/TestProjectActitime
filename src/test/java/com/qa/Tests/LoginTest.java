ca5191e557eac2a624bce565c074dc1cf4c44b57
sandeshgiri <sandesh.giri@gmail.com>
2018-06-21 23:32:23 +0530

    second commit

--- src/test/java/com/qa/Tests/LoginTest.java
+++ src/test/java/com/qa/Tests/LoginTest.java
@@ -14,20 +14,21 @@ public class LoginTest {
 	
 	WebDriver driver;
 	ChromeOptions options;
-	String appURL="https://devweb.sslproc.com/net/posweb/";
-	
+	String appURL="https://www.facebook.com";
+	String username="sandesh.giri@gmail.com";
+	String password="Indian_1";
 	
 	@BeforeMethod
 	public void setUp()
 	{
-		System.setProperty("webdriver.chrome.driver", "\\exeFiles\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "\\Java_Selenium\\DemoGitRepo\\exeFiles\\chromedriver.exe");
 		options=new ChromeOptions();
 		options.addArguments("--start-maximized");
 		options.addArguments("--disable-notifications");
 		options.addArguments("--disable-infobars");
 		options.setAcceptInsecureCerts(true);
 		driver=new ChromeDriver(options);
-		driver.get("https://www.facebook.com");
+		driver.get(appURL);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				
 	}
@@ -35,11 +36,11 @@ public class LoginTest {
 	@Test
 	public void verify()
 	{
-		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("sandesh.giri@gmail.com");
-		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("Indian_1");
-		
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
 		driver.findElement(By.xpath("//input[@value='Log In']")).click();
 	}
+	
 	
 	@AfterMethod
 	public void tearDown()
